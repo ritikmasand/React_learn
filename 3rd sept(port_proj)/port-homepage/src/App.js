e7047e5f5dee34883051097bc57d1feaabc2383a
ritikmasand <ritikmasand10@gmail.com>
2022-09-03 18:24:16 +0530

    3rd sept

---
+++ 3rd sept(port_proj)/port-homepage/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Bio from "./component/Bio";
+import Experience from "./component/Experience";
+import Skills from "./component/Skills";
+import Contact from "./component/Contact";
+
+function App() {
+  return (
+    <>
+      <Bio />
+      <hr />
+      <Experience />
+      <hr />
+      <Skills />
+      <hr />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
