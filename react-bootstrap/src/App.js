7d9d8903157c0cddc82792c889331379f9e4512b
ritikmasand <ritikmasand10@gmail.com>
2022-09-03 22:33:40 +0530

    react bootstrap

---
+++ react-bootstrap/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
+
+/*tasks
+you have to use a carousel component in your current react project
+export it by using a default export from the component file
+import that component in your app.js file
+*/
